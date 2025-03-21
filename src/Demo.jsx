e1b8d35d6e4889116a7b59b210ee9cfd1443529f
Tejaswi0504 <tejaswikommisetti2004@gmail.com>
2025-03-21 15:00:31 +0530

    created project

---
+++ src/Demo.jsx
@@ -0,0 +1,14 @@
+//import React from "react";
+
+function Demo()
+{
+    return (
+        <>
+        <p>KL University</p>
+        <p>CSE Department</p>
+        </>
+
+    )
+      
+}
+export default Demo
